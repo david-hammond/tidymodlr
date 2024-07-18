e3f91e197d94dc93feacfb1d907cee0d3b465416
david-hammond <anotherdavidhammond@gmail.com>
2024-07-19 09:53:45 +1000

    simplfied

--- R/tidymodl.R
+++ R/tidymodl.R
@@ -2,10 +2,9 @@
 #'
 #' Creates a model matrix style R6 class for modelling with long tidy data
 #' @importFrom R6 R6Class
-#' @importFrom dplyr select left_join all_of
+#' @importFrom dplyr select left_join all_of rename
 #' @importFrom tidyr pivot_wider pivot_longer complete
 #' @importFrom tm removePunctuation removeWords stopwords
-#' @importFrom dm decompose_table
 #' @importFrom rlang .data
 #' @param df A tidy long data frame
 #' @param pivot_column The column name on which the pivot will occur
@@ -13,10 +12,10 @@
 #' @examples
 #' data(wb)
 #' mdl <- tidymodl$new(wb,
-#'                     pivot_column = "indicator",
-#'                     pivot_value = "value")
+#'                    pivot_column = "indicator",
+#'                   pivot_value = "value")
 #' ### Use mdldata for modelling
-#' fit = lm(data = mdl$mdldata, gni ~ gcu + ppt)
+#' fit <- lm(data = mdl$child, gni ~ gcu + ppt)
 #'
 #' ### Can be used to add a yhat value for processed data
 #'
@@ -25,24 +24,24 @@
 #' ### NOT RUN
 #' # Use for xgboost imputation
 #' # library(mixgb)
-#' # imp = mixgb(mdl$mdldata, save.models = T)
-#' # tmp = mdl$reconstitute(imp$imputed.data[[1]])
+#' # imp <- mixgb(mdl$child, save.models = T)
+#' # tmp <- mdl$assemble(newdata = imp$imputed.data[[1]])
 #'
 #' ### NOT RUN
 #' # Use for mice imputation
 #' # library(mice)
-#' # imp = mice(as.data.frame(scale(mdl$matrix)), print = FALSE)
-#' # tmp = mdl$reconstitute(complete(imp))
+#' # imp <- mice(as.data.frame(scale(mdl$matrix)), print = FALSE)
+#' # tmp <- mdl$assemble(complete(imp))
 #'
 #' ### In this example we will just use dummy new data
-#' nc = ncol(mdl$mdldata)
-#' nr = nrow(mdl$mdldata)
-#' dm = nc*nr
-#' dummy = matrix(runif(dm),
+#' nc <- ncol(mdl$child)
+#' nr <- nrow(mdl$child)
+#' dm <- nc*nr
+#' dummy <- matrix(runif(dm),
 #'         ncol = nc) |>
 #'         data.frame()
-#' names(dummy) = names(mdl$mdldata)
-#' tmp = mdl$reconstitute(dummy)
+#' names(dummy) = names(mdl$child)
+#' tmp = mdl$assemble(dummy)
 #'
 
 #' @export
@@ -53,12 +52,12 @@ tidymodl <- R6::R6Class("tidymodl",
                             #' Creates a new instance of this [R6][R6::R6Class] class.
                             #' @field data (`data.frame()`)\cr
                             #'   The original tidy long data frame
-                            #' @field mdldata (`data.frame()`)\cr
+                            #' @field child (`data.frame()`)\cr
                             #'   The model matrix version of the data
                             lock_objects = FALSE,
                             public = list(
                               data = NULL,
-                              mdldata = NULL,
+                              child = NULL,
                               #' @description
                               #' Create a new tidymodl object.
                               #' @param df A tidy long data frame
@@ -68,31 +67,45 @@ tidymodl <- R6::R6Class("tidymodl",
                               initialize = function(df,
                                                     pivot_column,
                                                     pivot_value) {
-                                self$data = df
-                                tmp = .get_dm(df,
-                                              pivot_column,
-                                              pivot_value)
-                                self$mdldata = tmp$mat[,-1]
-                                private$key_ind = tmp$mat$id
-                                private$master = tmp$master
-                                private$key = tmp$key
+                                self$data <- df
+                                private$pivot_column <- pivot_column
+                                private$pivot_value <- pivot_value
+                                private$key <- private$.get_key()
+                                tmp <- private$.get_dm()
+                                self$child <- tmp$child
+                                private$parent <- tmp$parent
                               },
                               #' @description
                               #' Adds a results matrix
-                              #' @param m A results matrix
-                              reconstitute = function(m = NULL) {
-                                if(!is.null(m)){
-                                  df = private$master
-                                  m = as.data.frame(m)
-                                  m$id = private$key_ind
-                                  m = m |>
-                                    pivot_longer(cols = -id, names_to = "vid", values_to = "yhat")
-                                  df = df |> left_join(m, by = c("vid", "id"))
-                                  df = df[, c(names(self$data), "yhat")]
-                                }else{
-                                  df = self$data
+                              #' @param newdata A results matrix
+                              assemble = function(newdata = NULL) {
+                                parent <- private$parent |>
+                                  cbind(self$child)
+                                parent <- parent |>
+                                  pivot_longer(!eval(names(private$parent)),
+                                               names_to = "hashkey",
+                                               values_to = private$pivot_value) |>
+                                  left_join(private$key, by = 'hashkey') |>
+                                  select(-hashkey)
+                                parent <- parent[, c(names(self$data))]
+                                if(!is.null(newdata)){
+                                  child <- private$parent |>
+                                    cbind(newdata)
+                                  child <- child |>
+                                    pivot_longer(!eval(names(private$parent)),
+                                                 names_to = "hashkey",
+                                                 values_to = "yhat") |>
+                                    left_join(private$key, by = 'hashkey') |>
+                                    select(-hashkey)
+                                  child <- child[, c(setdiff(names(self$data),
+                                                             private$pivot_value),
+                                                'yhat')]
+                                  parent <- parent |> left_join(child,
+                                                               by = c(names(private$parent),
+                                                                      private$pivot_column))
+                                  parent <- parent[, c(names(self$data),'yhat')]
                                 }
-                                return(df)
+                                return(parent)
                               },
                               #' @description
                               #' Prints the key and the head matrix
@@ -100,13 +113,38 @@ tidymodl <- R6::R6Class("tidymodl",
                                 cat("Variable Key: \n")
                                 print(private$key)
                                 cat("Head Data Matrix: \n")
-                                print(head(self$mdldata, 5))
+                                print(head(self$child, 5))
                               }
                             ),
                         private = list(
                           master = NULL,
                           key_ind = NULL,
-                          key = NULL
+                          key = NULL,
+                          pivot_column = NULL,
+                          pivot_value = NULL,
+                          .get_key = function(){
+                            key = data.frame(variable = unique(self$data[,private$pivot_column]),
+                                             hashkey = apply(unique(self$data[,private$pivot_column]), 1, tolower))
+                            names(key)[1] <- private$pivot_column
+                            key$hashkey <- tm::removePunctuation(key$hashkey)
+                            key$hashkey <- tm::removeWords(key$hashkey, words = stopwords())
+                            key$hashkey <- abbreviate(key$hashkey, minlength = 3)
+                            key$hashkey <- make.unique(key$hashkey)
+                            return(key)
+                          },
+                          .get_dm = function(){
+                            parent_cols <- setdiff(names(self$data), c(private$pivot_column, private$pivot_value))
+                            df <- self$data |> left_join(private$key, by = private$pivot_column) |>
+                              select(-all_of(private$pivot_column))
+                            df <- df |>
+                              pivot_wider(names_from = hashkey,
+                                          values_from = eval(private$pivot_value))
+                            parent <- df |> select(eval(parent_cols) )
+                            child <- df |> select(-eval(parent_cols))
+                            tmp <- list(parent = parent, child = child)
+                            return(tmp)
+                          }
+
                         )
 )
 
@@ -118,15 +156,7 @@ tidymodl <- R6::R6Class("tidymodl",
 #'
 #' @keywords internal
 
-.get_key = function(tmp, pivot_column){
-  key = data.frame(variable = unique(tmp[,pivot_column]), vid = apply(unique(tmp[,pivot_column]), 1, tolower))
-  names(key)[1] = pivot_column
-  key$vid = tm::removePunctuation(key$vid)
-  key$vid = tm::removeWords(key$vid, words = stopwords())
-  key$vid = abbreviate(key$vid, minlength = 3)
-  key$vid = make.unique(key$vid)
-  return(key)
-}
+
 
 #' Get pivot_column key data frame
 #'
@@ -135,20 +165,5 @@ tidymodl <- R6::R6Class("tidymodl",
 #' @param pivot_value The column name of the values to be pivoted
 #'
 #' @keywords internal
-.get_dm = function(df, pivot_column, pivot_value){
-  obs_fields = paste0(".data$", setdiff(names(df), pivot_value))
-  cmd <- paste0("df <- df |> complete(", paste(obs_fields, collapse = ","), ")")
-  eval(parse(text = cmd))
-  key = .get_key(df, pivot_column)
-  df = df |> left_join(key, by = pivot_column) |>
-    select(-all_of(pivot_column))
-  obs_fields = setdiff(names(df), c("vid", pivot_value))
-  y = decompose_table(df, "id", all_of(obs_fields))
-  y$matrix = y$child_table |>
-    pivot_wider(names_from = .data$vid, values_from = eval(pivot_value))
-  df = df |> left_join(y$parent_table) |>
-    left_join(key, by = "vid")
-  tmp = list(mat = y$matrix, master = df, key = key)
-  return(tmp)
-}
+
 
--- tests/testthat/test-classes.R
+++ tests/testthat/test-classes.R
@@ -4,23 +4,40 @@ test_that("returns df", {
                        pivot_column = "indicator",
                        pivot_value = "value")
 
-  nc = ncol(mdl$mdldata)
-  nr = nrow(mdl$mdldata)
-  dm = nc*nr
+  nc <- ncol(mdl$child)
+  nr <- nrow(mdl$child)
+  dm <- nc*nr
   dummy = matrix(runif(dm),
            ncol = nc) |>
            as.data.frame()
-  names(dummy) = names(mdl$mdldata)
-  tmp = mdl$reconstitute(dummy)
+  names(dummy) = names(mdl$child)
+  tmp = mdl$assemble(dummy)
   expect_s3_class(tmp, "data.frame")
 })
 
-test_that("reconstitute works", {
+test_that("assemble works", {
   data(wb)
   mdl <- tidymodl$new(wb,
                       pivot_column = "indicator",
                       pivot_value = "value")
-  tmp = mdl$reconstitute(mdl$mdldata)
-  test = identical(tmp$value, tmp$yhat)
-  expect(test)
+  tmp <- mdl$assemble(mdl$child)
+  test <- identical(tmp$value, tmp$yhat)
+  expect_true(test)
+})
+
+test_that("assemble preserves data", {
+  data(wb)
+  mdl <- tidymodl$new(wb,
+                      pivot_column = "indicator",
+                      pivot_value = "value")
+  tmp <-  mdl$assemble()
+  tmp <- tmp[!is.na(tmp$value),]
+  tmp <- tmp |>
+    rename(yhat = value)
+  tmp <- tmp %>% left_join(mdl$data,
+                          by = c('iso3c',
+                                 'indicator',
+                                 'year'))
+  test <- all(tmp$yhat == tmp$value)
+  expect_true(test)
 })
